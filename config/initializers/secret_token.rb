018e7dfdff17ad6b31ed3b7e5836073f8a3aa530
sgmcb <sgm@sgmcb.com>
2013-01-03 15:44:40 -0800

    Fresh Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reflib::Application.config.secret_token = '45b8dc7735dd80018b5ce82dddc6279172a99b48ffb09a91c58e1c0d085d3fff4211460278a311701d9f981701b05cdf64e3d3b5e39cecbd0d744aee64e4e52c'
